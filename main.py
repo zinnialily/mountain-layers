3d095a3fa43901995d4e53fb00cb5b5b2d278664
zinnialily <zinnialily303@gmail.com>
2026-05-19 10:08:23 -0500

    testing frontend by editng for render

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 # use fast api to run basic process
 
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 import shutil
 import os
 
 from processing import generate_layers
-
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # later replace with your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+from fastapi.staticfiles import StaticFiles
+
+app.mount("/outputs", StaticFiles(directory="outputs"), name="outputs")
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
